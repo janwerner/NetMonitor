c7dc3868d9a6af75903a39fc9aa6ea9ad1b2c88b
janwerner <mail@janwerner.de>
2018-03-10 11:32:03 +0100

    Update Sample query.sql

--- Sample query.sql
+++ Sample query.sql
@@ -1,6 +1,7 @@
-SELECT 
-    datetime(`TimeStamp`,'unixepoch','localtime') AS 'Date/Time', 
-    `RemoteAddress`, `InterfaceAlias`, 
-    CASE WHEN `PingSucceeded` = '0' THEN 'FALSE' ELSE 'TRUE' END AS `PingSucceeded`, 
-    `RoundTripTime` 
+select 
+	datetime(`TimeStamp`,'unixepoch','localtime') AS 'Date/Time', 
+	`RemoteAddress`, `InterfaceAlias`, 
+	CASE WHEN `PingSucceeded` = '0' THEN 'FALSE' ELSE 'TRUE' END AS `PingSucceeded`, 
+	`RoundTripTime` 
 FROM NetMonitor
+ORDER BY `Date/Time` DESC
